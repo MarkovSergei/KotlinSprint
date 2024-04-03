54119fd4802ba9c35fe6cde09d385dea7512ebad
MarkovSergei <dzey1@bk.ru>
2024-04-03 22:50:45 +0800

    KotlinSprint-2-1 task

---
+++ src/main/kotlin/lesson_2/less2_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main(){
+    val allStudent = 4f
+    val student1Assessment = 3f
+    val student2Assessment = 4f
+    val student3Assessment = 3f
+    val student4Assessment = 5f
+
+    val middleAssessment: Float = (student1Assessment + student2Assessment + student3Assessment + student4Assessment)/allStudent
+
+    print(middleAssessment)
+
+}
