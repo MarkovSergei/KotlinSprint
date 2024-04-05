28d5c1daccd7cc81a758fb6ca4399a182971c660
MarkovSergei <dzey1@bk.ru>
2024-04-05 23:09:48 +0800

    KotlinSprint-2-5 task new

--- src/main/kotlin/lesson_2/less2_task5.kt
+++ src/main/kotlin/lesson_2/less2_task5.kt
@@ -3,12 +3,12 @@ import kotlin.math.pow
 
 
 fun main() {
-    val principal = 70000.0
-    val rate = 0.167
-    val years = 20.0
+    val principal = 70000
+    val percentage = 16.7
+    val years = 20
 
+    val rate = percentage / 100
     val deposit = principal * (1 + rate).pow(years)
-
     val formattedDeposit = String.format("%.3f", deposit)
 
     println(formattedDeposit)
