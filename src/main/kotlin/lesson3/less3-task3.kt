20e3f59d2f94efbe1cefc4489f66d3a4cdb2000e
MarkovSergei <dzey1@bk.ru>
2024-04-06 20:36:52 +0800

    KotlinSprint-3-3 task

---
+++ src/main/kotlin/lesson3/less3-task3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson3
+
+fun main() {
+    val number = 4
+    println("$number x 1 = ${number * 1}")
+    println("$number x 2 = ${number * 2}")
+    println("$number x 3 = ${number * 3}")
+    println("$number x 4 = ${number * 4}")
+    println("$number x 5 = ${number * 5}")
+    println("$number x 6 = ${number * 6}")
+    println("$number x 7 = ${number * 7}")
+    println("$number x 8 = ${number * 8}")
+    println("$number x 9 = ${number * 9}")
+}
