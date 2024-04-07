b7d8973da8eb48654b6473868614b09750f536e2
MarkovSergei <dzey1@bk.ru>
2024-04-07 22:16:06 +0800

    KotlinSprint-1-5 task new

--- src/main/kotlin/lesson_1/less1_task5.kt
+++ src/main/kotlin/lesson_1/less1_task5.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_1
-const val SECOND_IN_SPACE = 6480
+
 const val SECOND_IN_HOUR = 3600
 const val SECOND_IN_MINUTE = 60
-fun main(){
 
-    val hour = (SECOND_IN_SPACE/SECOND_IN_HOUR).toString().padStart(2, '0')
-    val minute = ((SECOND_IN_SPACE % SECOND_IN_HOUR) / SECOND_IN_MINUTE).toString().padStart(2, '0')
-    val second = (SECOND_IN_SPACE % SECOND_IN_MINUTE).toString().padStart(2, '0')
+fun main() {
+    val secondInSpace = 6480
+    val hour = (secondInSpace/SECOND_IN_HOUR)
+    val minute = ((secondInSpace % SECOND_IN_HOUR) / SECOND_IN_MINUTE)
+    val second = (secondInSpace % SECOND_IN_MINUTE)
 
-
-    println("$hour:$minute:$second")
+    val time = String.format("%02d:%02d:%02d", hour, minute, second)
+    println(time)
 }
 
