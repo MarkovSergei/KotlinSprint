7aec259a06782df3b4b301d9709de042c22d5e42
MarkovSergei <dzey1@bk.ru>
2024-04-15 23:04:24 +0800

    KotlinSprint-4-3 task NEW

--- src/main/kotlin/lesson4/less4_task3.kt
+++ src/main/kotlin/lesson4/less4_task3.kt
@@ -1,15 +1,23 @@
 package org.example.lesson4
 
+const val SUN_TODAY = true
+const val AWNING_OPEN = true
+const val AIR_HUMIDITY = 20
+const val SEASON = "зима"
+
 fun main() {
-    val weatherTodaySunny = true
-    val tentOpen = true
+    val isSunny = true
+    val isAwningOpen = true
     val airHumidity = 20
-    val season = "зима"
+    val season = "не зима"
 
-
-    println("Благоприятные ли условия сейчас для роста бобовых? " +
-            "${weatherTodaySunny == true && 
-                    tentOpen == true && 
-                    airHumidity == 20 && 
-                    season == "не зима"}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? " +
+                "${
+                    SUN_TODAY == isSunny &&
+                            AWNING_OPEN == isAwningOpen &&
+                            airHumidity == AIR_HUMIDITY &&
+                            SEASON == season
+                }"
+    )
 }
