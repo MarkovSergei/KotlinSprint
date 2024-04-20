ff33c8434a97d087e9d8effec265bf3550328308
MarkovSergei <dzey1@bk.ru>
2024-04-20 22:26:58 +0800

    KotlinSprint-5-3 task NEW

--- src/main/kotlin/lesson5/less5_task3.kt
+++ src/main/kotlin/lesson5/less5_task3.kt
@@ -6,11 +6,11 @@ const val WIN_NUMBER2 = 24
 fun main() {
     print("Введите первое число от 0 до 42: ")
     val userNumber1 = readln().toInt()
-    val usNum1 = userNumber1==WIN_NUMBER1 || userNumber1==WIN_NUMBER2
+    val usNum1 = userNumber1 == WIN_NUMBER1 || userNumber1 == WIN_NUMBER2
 
     print("Введите второе число от 0 до 42: ")
     val userNumber2 = readln().toInt()
-    val usNum2 = userNumber2==WIN_NUMBER1 || userNumber2==WIN_NUMBER2
+    val usNum2 = userNumber2 == WIN_NUMBER1 || userNumber2 == WIN_NUMBER2
 
     val winResult = usNum1 && usNum2
     val consolationPrize = usNum1 || usNum2
@@ -20,8 +20,7 @@ fun main() {
     else if (consolationPrize) {
         println("Вы выиграли утешительный приз!")
         println("Правильные числа $WIN_NUMBER2 и $WIN_NUMBER1")
-    }
-    else{
+    } else {
         println("Неудача")
         println("Правильные числа $WIN_NUMBER2 и $WIN_NUMBER1")
     }
