98f850c46158f5524fdb7606b98834b234064c00
MarkovSergei <dzey1@bk.ru>
2024-04-20 23:22:43 +0800

    KotlinSprint-6-2 task NEW

--- src/main/kotlin/lesson6/less6_task2.kt
+++ src/main/kotlin/lesson6/less6_task2.kt
@@ -2,13 +2,7 @@ package org.example.lesson6
 
 fun main() {
     print("На сколько секунд поставить таймер: ")
-    var secTimer = readln().toInt()
-    val second = secTimer
-
-    while (secTimer > 0) {
-        println("${secTimer--}")
-        Thread.sleep(1000)
-    }
-    println("Прошло $second секунд")
-    readLine()
+    val secTimer = readln().toInt()
+    Thread.sleep(secTimer.toLong() * 1000)
+    println("Прошло $secTimer секунд")
 }
