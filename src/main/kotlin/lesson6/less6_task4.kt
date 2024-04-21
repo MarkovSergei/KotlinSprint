b51b27a41433a6eb97621ba1ebc6f73e575b8459
MarkovSergei <dzey1@bk.ru>
2024-04-21 21:48:41 +0800

    KotlinSprint-6-4 task NEW

--- src/main/kotlin/lesson6/less6_task4.kt
+++ src/main/kotlin/lesson6/less6_task4.kt
@@ -1,17 +1,16 @@
 package org.example.lesson6
 
 fun main() {
-    println("Угадай число от 1 до 9 (у тебя 5 попыток): ")
+    print("Угадай число от 1 до 9 (у тебя 5 попыток): ")
     val randomNumber = (1..9).random()
     for (i in 5 downTo 1) {
         val userNumber = readln().toInt()
         if (userNumber == randomNumber) {
             println("Ты угадал! Это была великолепная игра!")
-            break
-        } else if (userNumber != randomNumber && i > 1) {
+            return
+        } else if (i > 1) {
             println("Неверно. Осталось попыток: ${i - 1}")
-        } else {
-            println("Ты проиграл. Было загадано число $randomNumber")
         }
     }
+    println("Ты проиграл. Было загадано число $randomNumber")
 }
