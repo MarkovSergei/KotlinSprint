d579975f49cc4aac207ce35d233a358970b3aa4d
MarkovSergei <dzey1@bk.ru>
2024-04-22 23:46:49 +0800

    KotlinSprint-7-5 task NEW

--- src/main/kotlin/lesson7/less7_task5.kt
+++ src/main/kotlin/lesson7/less7_task5.kt
@@ -1,20 +1,28 @@
 package org.example.lesson7
 
 const val MINIMUM_LENGTH_PASSWORD = 6
+const val FOR_DOWN_TO = 4
 
 fun main() {
+    val wordsList = ('a'..'z').toList()
+    val bigWordsList = ('A'..'Z').toList()
+    val numbersList = ('0'..'9').toList()
+
+    val allChars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    val allListRandom = "${wordsList.random()}${bigWordsList.random()}${numbersList.random()}"
+
     while (true) {
-        print("Введите длину пароля (более 6): ")
+        print("Введите длину пароля (более $MINIMUM_LENGTH_PASSWORD): ")
         val lengthPassword = readln().toInt()
 
         if (lengthPassword >= MINIMUM_LENGTH_PASSWORD) {
-            for (i in lengthPassword downTo 1) {
-                val password = listOf('a'..'z', '0'..'9', 'A'..'Z').random().random()
-                print("$password")
+            print(allListRandom)
+            for (i in lengthPassword downTo FOR_DOWN_TO) {
+                print("${allChars.random()}")
             }
             break
         } else {
-            println("Введите длину пароля больше 6 символов!")
+            println("Введите длину пароля больше $MINIMUM_LENGTH_PASSWORD символов!")
         }
     }
 }
