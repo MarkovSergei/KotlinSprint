09e9ff6cc1f147116e47152c0b6917b1c84049da
MarkovSergei <dzey1@bk.ru>
2024-04-22 22:38:13 +0800

    KotlinSprint-7-1 task NEW

--- src/main/kotlin/lesson7/less7_task1.kt
+++ src/main/kotlin/lesson7/less7_task1.kt
@@ -1,9 +1,15 @@
 package org.example.lesson7
 
+const val PASSWORD_SIM = 6
+
 fun main() {
 
-    for (i in 6 downTo 1) {
-        val password = listOf('a'..'z', '0'..'9').random().random()
-        print("$password")
+    val wordList = ('a'..'z').toList()
+    val numberList = ('0'..'9').toList()
+
+    for (i in PASSWORD_SIM downTo 4) {
+        val words = wordList.random()
+        val number = numberList.random()
+        print("$words$number")
     }
 }
