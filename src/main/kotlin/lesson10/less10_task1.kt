dc00d131a2b1afe59634cfbf3c8a414dd570d5f8
MarkovSergei <dzey1@bk.ru>
2024-05-09 14:32:36 +0800

    less10_task1

---
+++ src/main/kotlin/lesson10/less10_task1.kt
@@ -0,0 +1,21 @@
+package org.example.lesson10
+
+fun main() {
+    print("Ход игрока. Выпало: ")
+    val playerStep = generateRandomNumber()
+    println(playerStep)
+    print("Ход компьютера. Выпало: ")
+    val compStep = generateRandomNumber()
+    println(compStep)
+    if (playerStep > compStep) {
+        println("Победило человечество")
+    } else if (playerStep == compStep) {
+        println("Ничья")
+    } else {
+        println("Победила машина")
+    }
+}
+
+fun generateRandomNumber(): Int {
+    return (1..6).random()
+}
