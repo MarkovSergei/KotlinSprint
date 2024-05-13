c4715f005fc860a9b0e7df04a9a688fcd403e4eb
MarkovSergei <dzey1@bk.ru>
2024-05-14 00:10:35 +0800

    less10_task5 NEW1

--- src/main/kotlin/lesson10/less10_task5.kt
+++ src/main/kotlin/lesson10/less10_task5.kt
@@ -4,21 +4,21 @@ const val LOGIN = "логин"
 const val PASSWORD = "пароль"
 const val TOKEN_LENGTH = 32
 
-val symbolList = listOf(('0'..'9') + ('a'..'z') + ('A'..'Z'))
 val orderList = listOf("Товар1", "Товар2", "Товар3")
 
-
 fun main() {
-    val token = authorizesVerifiesGeneratesToken()
-    getOrderList(token)
-}
-
-fun authorizesVerifiesGeneratesToken(): String? {
     print("Введите логин: ")
     val login = readln()
 
     print("Введите логин: ")
     val password = readln()
+
+    val token = authorizesVerifiesGeneratesToken(login, password)
+    getOrderList(token)
+}
+
+fun authorizesVerifiesGeneratesToken(login: String?, password: String?): String? {
+    val symbolList = listOf(('0'..'9') + ('a'..'z') + ('A'..'Z'))
 
     return if (login == LOGIN && password == PASSWORD) {
         symbolList.random().shuffled().take(TOKEN_LENGTH).joinToString("")
