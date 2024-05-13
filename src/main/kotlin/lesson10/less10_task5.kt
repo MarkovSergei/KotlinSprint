bd2bc5a218ca1a2beea9afd4916e6868bd40ca72
MarkovSergei <dzey1@bk.ru>
2024-05-13 23:54:33 +0800

    less10_task5 NEW

--- src/main/kotlin/lesson10/less10_task5.kt
+++ src/main/kotlin/lesson10/less10_task5.kt
@@ -3,28 +3,34 @@ package org.example.lesson10
 const val LOGIN = "логин"
 const val PASSWORD = "пароль"
 const val TOKEN_LENGTH = 32
+
+val symbolList = listOf(('0'..'9') + ('a'..'z') + ('A'..'Z'))
 val orderList = listOf("Товар1", "Товар2", "Товар3")
+
 
 fun main() {
     val token = authorizesVerifiesGeneratesToken()
-    if (token != null) {
-        getOrderList(token)
-    } else {
-        println("Авторизация неудачная")
-    }
+    getOrderList(token)
 }
 
 fun authorizesVerifiesGeneratesToken(): String? {
     print("Введите логин: ")
     val login = readln()
+
     print("Введите логин: ")
     val password = readln()
+
     return if (login == LOGIN && password == PASSWORD) {
-        (1..TOKEN_LENGTH).map { ('A'..'z') + ('A'..'Z') + ('0'..'9').random() }.joinToString("")
+        symbolList.random().shuffled().take(TOKEN_LENGTH).joinToString("")
     } else {
         null
     }
-
 }
 
-fun getOrderList(token: String) = println(orderList)
+fun getOrderList(token: String?) {
+    if (token != null) {
+        println(orderList)
+    } else {
+        println("Авторизация неудачная")
+    }
+}
