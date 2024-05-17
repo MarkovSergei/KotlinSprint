2ed47c9d90f3b6b695ff27ac0120e81e324b8724
MarkovSergei <dzey1@bk.ru>
2024-05-18 00:19:15 +0800

    KotlinSprint-11-2

---
+++ src/main/kotlin/lesson11/less11_task2/less11_task2.kt
@@ -0,0 +1,45 @@
+package org.example.lesson11.less11_task2
+
+data class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String? = null,
+) {
+    fun userInfo() {
+        println("ID - $id, \nЛОГИН - $login,\nПАРОЛЬ - $password,\nПОЧТА - $email,\nБИОГРАФИЯ - $bio")
+    }
+
+    fun biography(): String {
+        print("Введите биографию: ")
+        return readln()
+    }
+
+    fun changePassword(): String {
+        print("Введите старый пароль: ")
+        val oldPassword = readln()
+        if (oldPassword == password) {
+            print("Введите новый пароль: ")
+            return readln()
+        } else {
+            println("Пароль неверный")
+            return password
+        }
+    }
+}
+
+fun main() {
+
+    val newUser = User2(
+        1,
+        "login",
+        "password",
+        "example@mail.ru",
+        )
+
+    newUser.bio = newUser.biography()
+    newUser.password = newUser.changePassword()
+    println()
+    newUser.userInfo()
+}
