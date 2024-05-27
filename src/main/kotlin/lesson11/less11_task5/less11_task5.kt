e8fac1c9d6aa6e7ba5a7eb011ec478f01a123981
MarkovSergei <dzey1@bk.ru>
2024-05-27 23:44:20 +0800

    KotlinSprint-11-5 NEW33

--- src/main/kotlin/lesson11/less11_task5/less11_task5.kt
+++ src/main/kotlin/lesson11/less11_task5/less11_task5.kt
@@ -1,43 +1,46 @@
 package org.example.lesson11.less11_task5
 
 fun main() {
-    val userId = 1
-    val userName = "Вася"
 
-    val newUser = Forum(userId, userName)
-    newUser.createNewUser(userId, userName)
+    val newForum = Forum()
+    newForum.createNewUser(1, "Вася", "12345")
+    newForum.createNewUser(2, "Петя", "67890")
+
+    newForum.createNewMessage(1, 1, "Привет!")
+    newForum.createNewMessage(2, 2, "Здорово")
+    newForum.createNewMessage(3, 1, "Как дела?")
+    newForum.createNewMessage(4, 2, "Нормально")
+
+    newForum.printThread()
 }
 
-class Forum(
-    userId: Int,
-    userName: String,
-) {
-    var membersList = mutableListOf<ForumMember>()
-    var messagesList = mutableListOf<ForumMessage>()
+class Forum {
+    private var membersList = mutableListOf<ForumMember>()
+    private var messagesList = mutableListOf<ForumMessage>()
 
-    fun createNewUser(userId: Int, userName: String) {
-        ForumMember.createNewUser(membersList, userId, userName)
+
+    fun createNewUser(userId: Int, userName: String, password: String) {
+        if (!membersList.any { it.userId == userId && it.userName == userName }) {
+            membersList.add(ForumMember(userId, userName, password))
+        }
     }
 
-    fun createNewMessage() {
+    fun createNewMessage(messageId: Int, authorId: Int, message: String) {
+        messagesList.add(ForumMessage(messageId, authorId, message))
     }
 
     fun printThread() {
-    }
-}
-
-class ForumMember(
-    val userId: Int,
-    val userName: String,
-) {
-    companion object {
-        fun createNewUser(membersList: MutableList<ForumMember>, userId: Int, userName: String) {
-            if (!membersList.any { it.userId == userId && it.userName == userName }) {
-                membersList.add(ForumMember(userId, userName))
-            }
+        messagesList.forEach { message ->
+            println("${message.messageId}: ${message.message}")
         }
     }
 }
+
+data class ForumMember(
+    val userId: Int,
+    val userName: String,
+    val password: String,
+)
 
 data class ForumMessage(
     val messageId: Int,
