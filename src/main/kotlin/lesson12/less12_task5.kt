73a62550135bda25d556cd092e615b575b2f8dcb
MarkovSergei <dzey1@bk.ru>
2024-06-08 23:09:25 +0800

    KotlinSprint-12-5 NEW

--- src/main/kotlin/lesson12/less12_task5.kt
+++ src/main/kotlin/lesson12/less12_task5.kt
@@ -1,38 +1,37 @@
 package org.example.lesson12
-import kotlin.math.roundToInt
-import kotlin.random.Random
 
 fun main() {
 
-    val dayWeatherMonthList =  List(30) { Random.nextInt(0, 21) }
-    val nightWeatherMonthList =  List(30) { Random.nextInt(-10, 21) }
-    val precipitationMonthList =  List(30) { Random.nextBoolean() }
+    val weatherValueList = mutableListOf<WeatherValueClass>()
+    val tempValue = (-30..30)
 
+    for (i in 30 downTo 1) {
+        weatherValueList.add(
+            WeatherValueClass(
+                tempValue.random(),
+                tempValue.random(),
+                when ((0..1).random()) {
+                    1 -> true
+                    else -> false
+                }
+            )
+        )
+    }
+    val listDayTemp = weatherValueList.map { it.dayTemperature }
+    val listOfNightTemp = weatherValueList.map { it.nightTemperature }
+    val listPrecipitation = weatherValueList.map { it.isPrecipitation }.filter { it }
 
-    val weatherData5 = WeatherDataKelvin11(
-        dayWeatherMonthList,
-        nightWeatherMonthList,
-        precipitationMonthList
+    println(
+        """
+        Средняя температура днём: ${listDayTemp.average().toInt()}
+        Средняя температура ночью: ${listOfNightTemp.average().toInt()}
+        Дней с осадками: ${listPrecipitation.count()}     
+    """.trimIndent()
     )
 }
 
-class WeatherDataKelvin11(
-    dayWeatherMonthList: List<Int>,
-    nightWeatherMonthList: List<Int>,
-    precipitationMonthList: List<Boolean>,
-) {
-    private val middleDayTemperature = dayWeatherMonthList.average().roundToInt()
-    private val middleNightTemperature = nightWeatherMonthList.average().roundToInt()
-    private val sumPrecipitationInMonth = precipitationMonthList.countTrue()
-
-    init {
-        println("Средняя Температура днём: $middleDayTemperature °C")
-        println("Средняя Температура ночью: $middleNightTemperature °C")
-        println("Количество дней с осадками: $sumPrecipitationInMonth")
-        println()
-    }
-
-    private fun List<Boolean>.countTrue(): Int {
-        return this.filter { it }.size
-    }
-}
+class WeatherValueClass(
+    val dayTemperature: Int,
+    val nightTemperature: Int,
+    val isPrecipitation: Boolean
+)
