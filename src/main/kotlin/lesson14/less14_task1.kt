051932197b1167ca81c6784a084e9936517330cf
MarkovSergei <dzey1@bk.ru>
2024-06-10 22:14:18 +0800

    KotlinSprint-14-1 NEW11

--- src/main/kotlin/lesson14/less14_task1.kt
+++ src/main/kotlin/lesson14/less14_task1.kt
@@ -2,42 +2,47 @@ package org.example.lesson14
 
 fun main() {
 
-    val liner1 = Liner("Liner1", 50, 1000)
+    val liner1 = Liner("Liner1")
+    val cargo1 = Cargo("Cargo1")
+    val icebreaker1 = Icebreaker("Icebreaker1")
+
     liner1.printInfo()
-    val cargo1 = Cargo("Cargo1", 25, 100, 3000)
     cargo1.printInfo()
-    val icebreaker1 = Icebreaker("Icebreaker1", 12, 50)
     icebreaker1.printInfo()
 }
 
 open class Liner(
-    val name: String,
-    val speed: Int,
-    val capacity: Int,
+    open val name: String,
 ) {
+    open val speed: Int = 50
+    open val capacity: Int = 1000
+
     open fun printInfo() {
-        println("Имя коробля - $name, Скорость - $speed, Пассажировместимость - $capacity")
+        println("Имя корабля - $name, Скорость - $speed, Пассажировместимость - $capacity")
     }
 }
 
 class Cargo(
     name: String,
-    speed: Int,
-    capacity: Int,
-    private val carrying: Int,
-) : Liner(name, speed, capacity) {
+
+) : Liner(name) {
+    private val carrying = 3000
+    override val speed = 25
+    override val capacity = 100
+
     override fun printInfo() {
-        println("Имя коробля - $name, Скорость - $speed, Пассажировместимость - $capacity, Грузоподъемность - $carrying")
+        println("Имя корабля - $name, Скорость - $speed, Пассажировместимость - $capacity, Грузоподъемность - $carrying")
     }
 }
 
 class Icebreaker(
     name: String,
-    speed: Int,
-    capacity: Int,
     private val iceBreak: Boolean = true,
-) : Liner(name, speed, capacity) {
+) : Liner(name,) {
+    override  val speed = 12
+    override  val capacity = 50
+
     override fun printInfo() {
-        println("Имя коробля - $name, Скорость - $speed, Пассажировместимость - $capacity, Ломает лед - $iceBreak")
+        println("Имя корабля - $name, Скорость - $speed, Пассажировместимость - $capacity, Ломает лед - $iceBreak")
     }
 }
