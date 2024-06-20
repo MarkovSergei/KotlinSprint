00b51edf06571923dd3e8897484208c965b3d5f3
MarkovSergei <dzey1@bk.ru>
2024-06-20 21:53:46 +0800

    KotlinSprint-15-2 new

--- src/main/kotlin/lesson15_task2/less15_task2.kt
+++ src/main/kotlin/lesson15_task2/less15_task2.kt
@@ -5,13 +5,10 @@ fun main() {
     weatherServer.addWeatherData(WeatherServer.Temperature(30))
     weatherServer.addWeatherData(WeatherServer.PrecipitationAmount(50))
     weatherServer.addWeatherData(WeatherServer.Temperature(10))
-
-    weatherServer.sendMessage()
 }
 
 abstract class WeatherStationStats {
     abstract fun getData(): String
-    abstract fun getType(): String
 }
 
 class WeatherServer {
@@ -19,29 +16,10 @@ class WeatherServer {
 
     fun addWeatherData(data: WeatherStationStats) {
         weatherData.add(data)
-    }
 
-    fun sendMessage() {
-        when (weatherData.first().getType()) {
-            "temperature" -> {
-                for (data in weatherData) {
-                    if (data is Temperature) {
-                        println(data.getData())
-                    }
-                }
-            }
-
-            "precipitation" -> {
-                for (data in weatherData) {
-                    if (data is PrecipitationAmount) {
-                        println(data.getData())
-                    }
-                }
-            }
-
-            else -> {
-                println("Неизвестное сообщение")
-            }
+        when (data) {
+            is Temperature -> println(data.getData())
+            is PrecipitationAmount -> println(data.getData())
         }
     }
 
@@ -49,19 +27,11 @@ class WeatherServer {
         override fun getData(): String {
             return "$amount мм осадков"
         }
-
-        override fun getType(): String {
-            return "precipitation"
-        }
     }
 
     class Temperature(private val temp: Int) : WeatherStationStats() {
         override fun getData(): String {
             return "$temp градусов Цельсия"
-        }
-
-        override fun getType(): String {
-            return "temperature"
         }
     }
 }
