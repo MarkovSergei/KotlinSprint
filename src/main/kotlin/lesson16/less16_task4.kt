f76cb324ee6c81188c081b2a8878a6d48c18f229
MarkovSergei <dzey1@bk.ru>
2024-06-27 16:23:56 +0800

    KotlinSprint-16-4 new

--- src/main/kotlin/lesson16/less16_task4.kt
+++ src/main/kotlin/lesson16/less16_task4.kt
@@ -2,18 +2,18 @@ package org.example.lesson16
 
 fun main() {
     val newOrder = Order()
-    newOrder.changeStatus(managerSend("Получен"))
-}
-
-fun managerSend(changeStatusOrder: String): String {
-    return changeStatusOrder
+    newOrder.managerSend("получен")
 }
 
 class Order {
     private val idOrder = 431
     private var orderStatus = "отправлен"
 
-    fun changeStatus(orderStatusNew: String) {
+    fun managerSend(changeStatusOrder: String) {
+        changeStatus(changeStatusOrder)
+    }
+
+    private fun changeStatus(orderStatusNew: String) {
         orderStatus = orderStatusNew
         println("Новый статус заказа № $idOrder: $orderStatus")
     }
