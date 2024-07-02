8390161c6b88a60aa542957e558032eabaad33b0
MarkovSergei <dzey1@bk.ru>
2024-07-02 10:33:22 +0800

    KotlinSprint-17-4

--- src/main/kotlin/lesson17/less17_task4.kt
+++ src/main/kotlin/lesson17/less17_task4.kt
@@ -4,16 +4,16 @@ fun main() {
     val newParcel = Parcel(1, "Москва")
     newParcel.parcelLocation = "Тверь"
     newParcel.parcelLocation = "Пемза"
+    newParcel.parcelLocation = "Иркутск"
     newParcel.printInfo()
-
 }
 
 class Parcel(
     private val parcelId: Int,
-    var parcelLocation: String,
+    location: String,
 ) {
-    private var locationCount = 1
-    var location: String = parcelLocation
+    private var locationCount = 0
+    var parcelLocation: String = location
         set(value) {
             if (value != parcelLocation) {
                 locationCount++
