5d854492326f01354038bc070f78f3228c4cc843
MarkovSergei <dzey1@bk.ru>
2024-07-07 22:04:00 +0800

    KotlinSprint-18-4

--- src/main/kotlin/lesson18/less18_task4.kt
+++ src/main/kotlin/lesson18/less18_task4.kt
@@ -8,13 +8,13 @@ fun main() {
     println("Площадь поверхности упаковки куба: ${cubePackage.calculateSurfaceArea()}")
 }
 
-open class Package(
-    private val length: Double,
-    private val width: Double,
-    private val height: Double
+abstract class Package(
+    val length: Double,
+    val width: Double,
+    val height: Double
 ) {
     open fun calculateSurfaceArea(): Double {
-        return 2 * (length * width + length * height + width * height)
+        return 0.0
     }
 }
 
@@ -22,9 +22,17 @@ class RectangularPackage(
     length: Double,
     width: Double,
     height: Double
-) : Package(length, width, height)
+) : Package(length, width, height) {
+    override fun calculateSurfaceArea(): Double {
+        return 2 * (length * width + length * height + width * height)
+    }
+}
 
 class CubePackage(
-    sideLength: Double
-) : Package(sideLength, sideLength, sideLength)
+    private val sideLength: Double
+) : Package(sideLength, sideLength, sideLength) {
+    override fun calculateSurfaceArea(): Double {
+        return 6 * sideLength * sideLength
+    }
+}
 
