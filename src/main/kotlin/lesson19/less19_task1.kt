ab06aa1d69207f1040105070e4f42aae6182b665
MarkovSergei <dzey1@bk.ru>
2024-07-16 23:08:09 +0800

    KotlinSprint-19-1

--- src/main/kotlin/lesson19/less19_task1.kt
+++ src/main/kotlin/lesson19/less19_task1.kt
@@ -1,25 +1,17 @@
 package org.example.lesson19
 
-enum class Fish {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESEFIGHTINGFISH,
-}
-
-fun setFish(fish: Fish) {
-    when (fish) {
-        Fish.GUPPY -> println("Гуппи")
-        Fish.ANGELFISH -> println("скалярия")
-        Fish.GOLDFISH -> println("золотая рыбка")
-        Fish.SIAMESEFIGHTINGFISH -> println("петушок")
-    }
-}
-
 fun main() {
     println("Доступные виды рыб:")
-    val fishList = listOf(Fish.GUPPY, Fish.ANGELFISH, Fish.GOLDFISH, Fish.SIAMESEFIGHTINGFISH)
-    fishList.forEach { fish ->
-        setFish(fish)
+    val fishList = Fish.entries.toTypedArray()
+    for (fish in fishList) {
+        println(fish.fishName)
     }
 }
+
+enum class Fish(var fishName: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("скалярия"),
+    GOLDFISH("золотая рыбка"),
+    SIAMESEFIGHTING("петушок"),
+}
+
