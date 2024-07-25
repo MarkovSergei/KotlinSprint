afb47e3ce3d441208a176b4d2282f7d125f9f7ba
MarkovSergei <dzey1@bk.ru>
2024-07-25 23:03:58 +0800

    KotlinSprint-20-4

--- src/main/kotlin/lesson20/less20_task4.kt
+++ src/main/kotlin/lesson20/less20_task4.kt
@@ -2,14 +2,7 @@ package org.example.lesson20
 
 fun main() {
     val items = listOf("Яблоко", "Апельсин", "Груша", "Банан", "Персик")
-    val clickedItems = items.mapIndexed { index, item ->
-        if (index % 2 != 0) {
-            { println("Нажат элемент $item") }
-        } else {
-            {}
-        }
-    }
-    for (clickedItem in clickedItems) {
-        clickedItem()
+    items.filterIndexed { index, _ -> index % 2 != 0 }.map { item ->
+        println("Нажат элемент $item")
     }
 }
