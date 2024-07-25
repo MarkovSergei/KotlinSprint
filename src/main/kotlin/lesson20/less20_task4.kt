42801fbbbc6ea4cf1a7b56ab3e1702bf2cc5c914
MarkovSergei <dzey1@bk.ru>
2024-07-25 23:12:00 +0800

    KotlinSprint-20-4

--- src/main/kotlin/lesson20/less20_task4.kt
+++ src/main/kotlin/lesson20/less20_task4.kt
@@ -1,6 +1,7 @@
 package org.example.lesson20
 
 fun main() {
+
     val items = listOf("Яблоко", "Апельсин", "Груша", "Банан", "Персик")
     items.filterIndexed { index, _ -> index % 2 != 0 }.map { item ->
         println("Нажат элемент $item")
