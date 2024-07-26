5c94ec90d6c74835193136b456c0a35f277aedf5
MarkovSergei <dzey1@bk.ru>
2024-07-26 18:04:24 +0800

    KotlinSprint-21-4

--- src/main/kotlin/lesson21/less21_task4.kt
+++ src/main/kotlin/lesson21/less21_task4.kt
@@ -1,5 +1,17 @@
 package org.example.lesson21
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val fileName = "dict.txt"
+    val word = "Hello"
+
+    val file = File(fileName)
+    file.writeWordToFile(word)
+}
+
+fun File.writeWordToFile(word: String) {
+    word.lowercase().let {
+        this.writeText(it)
+    }
 }
